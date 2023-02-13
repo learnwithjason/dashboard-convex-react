84e400e9c9f3992b0a8ecfda1230b0a30804440c
Jason Lengstorf <jason@lengstorf.com>
2023-02-12 22:18:37 -0800

    feat: add seed data if the DB is unpupped

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,38 +1,6 @@
 import { useQuery, useMutation } from '../../convex/_generated/react';
 import { reactionTypes } from './bar-chart';
-
-const seedPuppers = [
-  {
-    name: 'Floof',
-    photo:
-      'https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=faces',
-  },
-  {
-    name: 'Bootsie',
-    photo:
-      'https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=entropy',
-  },
-  {
-    name: 'Daisy',
-    photo:
-      'https://images.unsplash.com/photo-1596492784531-6e6eb5ea9993?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=top',
-  },
-  {
-    name: 'Scraps',
-    photo:
-      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=entropy',
-  },
-  {
-    name: 'Pickles',
-    photo:
-      'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500',
-  },
-  {
-    name: 'Moira',
-    photo:
-      'https://images.unsplash.com/photo-1588269845464-8993565cac3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=top',
-  },
-];
+import { seedData } from '../util/seed-pups';
 
 export function List() {
   const pups = useQuery('pups:get');
@@ -40,6 +8,10 @@ export function List() {
 
   if (!pups) {
     return null;
+  }
+
+  if (Array.isArray(pups) && pups.length === 0) {
+    seedData();
   }
 
   return (
---
+++ src/util/seed-pups.ts
@@ -0,0 +1,42 @@
+import { useMutation } from '../../convex/_generated/react';
+
+const seedPups = [
+  {
+    name: 'Floof',
+    photo:
+      'https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=faces',
+  },
+  {
+    name: 'Bootsie',
+    photo:
+      'https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=entropy',
+  },
+  {
+    name: 'Daisy',
+    photo:
+      'https://images.unsplash.com/photo-1596492784531-6e6eb5ea9993?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=top',
+  },
+  {
+    name: 'Scraps',
+    photo:
+      'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=entropy',
+  },
+  {
+    name: 'Pickles',
+    photo:
+      'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500',
+  },
+  {
+    name: 'Moira',
+    photo:
+      'https://images.unsplash.com/photo-1588269845464-8993565cac3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80&h=500&crop=top',
+  },
+];
+
+export function seedData() {
+  const addPup = useMutation('pups:add');
+
+  seedPups.forEach((pup) => {
+    addPup(pup.name, pup.photo);
+  });
+}
