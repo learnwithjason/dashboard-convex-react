9f98c3b214a07be3bd967c633361790a6282646b
Jason Lengstorf <jason@lengstorf.com>
2023-02-20 15:13:44 -0800

    chore: whitespace

--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -3,26 +3,26 @@ import { List } from './list';
 import { BarChart } from './bar-chart';
 
 export function Toggle() {
-  const [state, setState] = useState<'main' | 'stats'>('main');
-  const nextState = state === 'main' ? 'stats' : 'main';
+	const [state, setState] = useState<'main' | 'stats'>('main');
+	const nextState = state === 'main' ? 'stats' : 'main';
 
-  return (
-    <div className="main">
-      <button onClick={() => setState(nextState)} className="toggle">
-        switch to {nextState} view
-      </button>
+	return (
+		<div className="main">
+			<button onClick={() => setState(nextState)} className="toggle">
+				switch to {nextState} view
+			</button>
 
-      {state === 'main' ? (
-        <>
-          <h1>click an emoji to react to these pups</h1>
-          <List />
-        </>
-      ) : (
-        <>
-          <h1>reaction stats for pups</h1>
-          <BarChart />
-        </>
-      )}
-    </div>
-  );
+			{state === 'main' ? (
+				<>
+					<h1>click an emoji to react to these pups</h1>
+					<List />
+				</>
+			) : (
+				<>
+					<h1>reaction stats for pups</h1>
+					<BarChart />
+				</>
+			)}
+		</div>
+	);
 }
